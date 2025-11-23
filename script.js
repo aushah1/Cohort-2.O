635f3dcee316027c70511f1c00f36cb3925833f0
Aushah Gowhar <waniaushahgowhar@gmail.com>
2025-11-23 10:25:36 +0530

    added some classes

--- script.js
+++ script.js
@@ -9,6 +9,23 @@ keys.forEach((key) => {
 });
 
 document.body.addEventListener("keydown", (dets) => {
+  keys.forEach((key) => {
+    if (key.id === dets.key && key.classList.contains("w-key")) {
+      key.style.backgroundColor = "rgb(226, 222, 222)";
+      key.style.transform = "scale(0.98)";
+      setTimeout(() => {
+        key.style.backgroundColor = "white";
+        key.style.transform = "scale(1)";
+      }, 300);
+    } else if (key.id === dets.key && key.classList.contains("b-key")) {
+      key.style.backgroundColor = "rgb(43, 43, 43)";
+      key.style.transform = "scale(0.98)";
+      setTimeout(() => {
+        key.style.backgroundColor = "black";
+        key.style.transform = "scale(1)";
+      }, 300);
+    }
+  });
   let audio = new Audio(`/audio/${dets.key}.mp3`);
   if (audio) {
     audio.play();

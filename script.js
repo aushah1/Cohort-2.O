e2608f8317ad82e24866c7887f2e2825e7869933
Aushah Gowhar <waniaushahgowhar@gmail.com>
2025-11-29 21:55:58 +0530

    card effect

--- script.js
+++
@@ -1,7 +0,0 @@
-addEventListener("mousemove", (dets) => {
-  console.log(dets.clientX, dets.clientY);
-  document.body.style.setProperty("--x", dets.clientX + "px");
-  document.body.style.setProperty("--y", dets.clientY + "px");
-  document.querySelector(".cursor").style.top = dets.clientY + "px";
-  document.querySelector(".cursor").style.left = dets.clientX + "px";
-});
